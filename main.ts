284c51ebff9bb3a2b159a16a3aad16142a0afc6c
ramen15 <72809089+ramen15@users.noreply.github.com>
2020-10-27 11:26:05 -0400

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
-
+light.setBrightness(10)
+light.setPixelColor(0, light.rgb(255, 165, 0))
+light.setPixelColor(1, light.rgb(255, 255, 0))
+light.setPixelColor(2, light.rgb(0, 255, 0))
+light.setPixelColor(3, light.rgb(0, 255, 255))
+light.setPixelColor(4, light.rgb(0, 180, 255))
+light.setPixelColor(5, light.rgb(0, 100, 255))
+light.setPixelColor(6, light.rgb(0, 70, 255))
+light.setPixelColor(7, light.rgb(50, 0, 200))
+light.setPixelColor(8, light.rgb(255, 0, 200))
+light.setPixelColor(9, light.rgb(255, 0, 110))
+while (true) {
+    
+    light.setPixelColor(0, light.rgb(255, 165, 0))
+    light.setPixelColor(1, light.rgb(255, 255, 0))
+    light.setPixelColor(2, light.rgb(0, 255, 0))
+    light.setPixelColor(3, light.rgb(0, 255, 255))
+    light.setPixelColor(4, light.rgb(0, 180, 255))
+    light.setPixelColor(5, light.rgb(0, 100, 255))
+    light.setPixelColor(6, light.rgb(0, 70, 255))
+    light.setPixelColor(7, light.rgb(50, 0, 200))
+    light.setPixelColor(8, light.rgb(255, 0, 200))
+    light.setPixelColor(9, light.rgb(255, 0, 110))
+    pause(3000)
+    light.clear()
+    pause(2000)
+}
